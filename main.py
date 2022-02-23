73f03209fcb1d53a25b45b8fe7fd0c0fd2ac0513
Code1Tech <78060856+Code1Tech@users.noreply.github.com>
2022-02-22 21:10:08 -0800

    👀 In-depth version of the code snippet.

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ url = requests.get("https://code1tech.repl.co")
 # Initialize the Scraper class.
 #  URL HTML (x.content recommened) | The parser to use (bs4) | Warnings mode turns off exceptions. Default is "False".
 Scraper = ezscrape.Scraper(url.content, 'html.parser', warningsmode=True)
+# "sfaewac" is the short form for SearchForAllElementsWithAClass()
 oofclass = Scraper.sfaewac('oof')
+#                class name ^^^
 
-# Sets off a warning.
+# Sets off a warning because the "hello" ID is not on the website.
 test = Scraper.sfaewai("hello")
 
 oofclass = str(oofclass)
